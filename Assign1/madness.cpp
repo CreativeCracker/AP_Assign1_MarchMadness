5d4cd50e55e8dbfeffc184370b75ddca084d1f8c
root <root@localhost.localdomain>
2015-03-08 20:37:07 +0500

    Resolved parsing and vectorisation.

--- Assign1/madness.cpp
+++ Assign1/madness.cpp
@@ -1,13 +1,17 @@
+#pragma chala de compiler bhaiya!
+
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
 #include <vector>
 #include <tr1/unordered_map>
 
 using namespace std;
 using namespace std::tr1;
 
-typedef unordered_map <string, vector<string> > mer_map;
+typedef vector< vector<string> > vect;
+typedef unordered_map <string,string> mer;
 
 void Tokenize(const string& str,vector<string>& tokens,const string& delimiters = " ")
 {
@@ -20,42 +24,63 @@ void Tokenize(const string& str,vector<string>& tokens,const string& delimiters 
     {
         // Found a token, add it to the vector.
         tokens.push_back(str.substr(lastPos, pos - lastPos));
-	cout<< str.substr(lastPos, pos - lastPos)<<"\t";
+	//cout<< str.substr(lastPos, pos - lastPos)<<"\t";
         // Skip delimiters.  Note the "not_of"
         lastPos = str.find_first_not_of(delimiters, pos);
         // Find next "non-delimiter"
         pos = str.find_first_of(delimiters, lastPos);
     }
-	cout<<endl<<endl;
+    //cout<<tokens.size()<<"\t";
+	//cout<<endl<<endl;
 }	
 
-mer_map *map(const char *file, mer_map *exp){
-	cout<<endl<<endl<<endl;
+void vectorise(const char *file, vect& exp, const char &b){
+	cout<<endl<<endl;
 	ifstream f;
 	f.open(file);
-	string omit;
+	string omit,line;
+	getline(f,omit, '\n');
+
+	while(f.good()){
+		//cout<<lin;
+		getline(f,line);
+
+		vector <string> vec;
+        
+            //read value
+	    //getline(f, value, '\n');
+	    //cout<<line<<"\n";
+
+	    Tokenize(line,vec,",");
+	    if(vec[0] == &b)
+            exp.push_back(vec);
+        
+	}
+	//cout<<exp.size()<<endl;
+	f.close();
+
+}
+
+
+void map(const char *file, mer& umap){
+	ifstream f;
+	f.open(file);
+	string omit,lin,key;
 	getline(f,omit, '\n');
 
 	while(f.good()){
 		int i=0;
 		string key, value;
-		vector <string> vec;
-            //try to read key, if there is none, break
-            if (!getline(f, key, ',')){
-			 break;
-	    }
-            //read value
-	    
-            getline(f, value, '\n');
-
-	    Tokenize(value,vec,",");
-	    //cout<<vec[0]<<endl;
-            (*exp)[key] = vec;
-            
+	//try to read key, if there is none, break
+		if (!getline(f, key, ',')){
+			break;
+		}
+	//read value
+		getline(f, value, '\n');
+		umap[key] = value;
 	}
-	
+
 	f.close();
-	return exp;
 }
 
 int main(int argc, char *argv[]){
@@ -66,19 +91,20 @@ int main(int argc, char *argv[]){
 	}
 
 	
-	mer_map  mymap,mymap2,mymap3,mymap4;
-	
-	map("teams.csv", &mymap);
-	map("seasons.csv", &mymap2);
-	map("tourney_results.csv", &mymap3);
-	map("regular_season_results.csv", &mymap4);
+	mer  mymap1,mymap2;
+	vect myvect1, myvect2;
+	const char season = *argv[1];
+	cout<<season<<endl;
 
+	map("teams.csv",mymap1);
+	map("seasons.csv",mymap2);
+	vectorise("regular_season_results.csv", myvect1,season);
+	vectorise("tourney_results.csv", myvect2,season);
 
-	
-	cout <<mymap.size()<< endl;
+	cout <<myvect1.size()<< endl;
+	cout <<myvect2.size()<< endl;
+	cout <<mymap1.size()<< endl;
 	cout <<mymap2.size()<< endl;
-	cout <<mymap3.size()<< endl;
-	cout <<mymap4.size()<< endl;
 
 
 	return 0;
